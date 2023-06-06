5f47c763db76758843acdbe006f008eeb558bf69
Murat <kavakmuratt@gmail.com>
2023-06-06 20:32:07 +0300

    insan class commit

---
+++ insan.py
@@ -0,0 +1,48 @@
+class Insan():
+    def __init__(self):
+        self.__ad=str()
+        self.__soyad=str()
+        self.__tc_no=str()
+        self.__yas=str()
+        self.__cinsiyet=str()
+        self.__uyruk=str()
+
+    def __str__(self):
+        text = "İsim : {}\nSoyisim:{}\nT.C. Kimlik Numarası : {}\nYas : {}\nCinsiyet : {}\nUyruk : {}".format(self.__ad,self.__soyad,self.__tc_no,self.__yas,self.__cinsiyet,self.__uyruk)
+        return text
+
+    def get_ad(self):
+        return self.__ad
+
+    def set_ad(self, isim):
+        self.__ad = isim
+
+    def get_soyad(self):
+        return self.__soyad
+
+    def set_soyad(self, soyisim):
+        self.__soyad = soyisim
+
+    def get_tcno(self):
+        return self.__tc_no
+
+    def set_tcno(self, tcnumara):
+        self.__tc_no = tcnumara
+
+    def get_yas(self):
+        return self.__yas
+
+    def set_yas(self, yeniyas):
+        self.__yas = yeniyas
+
+    def get_cinsiyet(self):
+        return self.__cinsiyet
+
+    def set_cinsiyet(self, gender):
+        self.__cinsiyet = gender
+
+    def get_uyruk(self):
+        return self.__uyruk
+
+    def set_uyruk(self, nationanlity):
+        self.__uyruk = nationanlity
